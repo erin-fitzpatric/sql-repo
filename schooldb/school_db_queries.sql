56694302fc80383b1562e1be90089a7d0ec99ad7
erin-fitzpatric <efitzpatric@gmail.com>
2019-10-03 15:21:37 -0400

    10/3 changes

---
+++ schooldb/school_db_queries.sql
@@ -0,0 +1,54 @@
+-- select all student
+select * from student;
+
+-- select all course
+select * from course;
+
+-- select all students without middle names (null)
+select * from student
+	where middleInitial is null; 
+
+-- How many students in OH? (count function)
+select count(*) from student
+	where state = 'OH'; 
+
+-- How many studnets per state?
+select state, count(*)
+	from student
+    group by state;
+
+-- Select oldest student ???
+select concat(firstName, " ", middleInitial, " ", lastName) as 'Oldest Student:', birthday as 'Birthday:'
+  from student
+ where birthday = (select min(birthday) from student);
+ 
+ -- Age of oldest student
+select birthday, year(birthday), CURDATE(), year(curdate()),
+	(year(curdate()) - year(birthday)) as Age
+    from student
+    order by age;
+    
+     -- Age of oldest student (to the date)
+select firstname, lastname, 
+		(year(curdate()) - year(birthday)) as Age
+	from student
+    where birthday = (select min(birthday) from student);
+
+-- Select youngest student
+select concat(firstName, " ", middleInitial, " ", lastName) as 'Oldest Student:', birthday as 'Birthday:'
+  from student
+ where birthday = (select max(birthday) from student);
+    
+-- How many social studies courses ???
+select count(*) as 'Social Studies' from course
+where subject = 'socialStudies';
+
+-- list all subjects starting with the letter 'm'
+select * from course
+where subject like 'm%';
+
+-- How many math courses?
+select count(*) as 'Math' from course
+where subject = 'math';
+
+-- Inner join of all 3 (student, enrolled, course)
